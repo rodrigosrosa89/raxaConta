59ec9cb2cc3cefadcf7a52b93b059f8a8eeda952
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2020-10-31 18:49:26 -0300

    Commit agrupando widgtes com Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,70 @@
 import 'package:flutter/material.dart';
-    
-class  extends StatefulWidget {
-  @override
-  _State createState() => _State();
+
+void main() {
+  runApp(MaterialApp(
+    home: paginaInicial(),
+  ));
 }
 
-class _State extends State<> {
+class paginaInicial extends StatefulWidget {
+  @override
+  _paginaInicialState createState() => _paginaInicialState();
+}
+
+class _paginaInicialState extends State<paginaInicial> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Raxa conta"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Form(
+          child: (Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(
+                Icons.monetization_on,
+                size: 120,
+                color: Colors.blue,
+              ),
+              TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Quantidade de pessoas",
+                      labelStyle: TextStyle(color: Colors.blue)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blue, fontSize: 18)),
+              TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Valor da conta",
+                      labelStyle: TextStyle(color: Colors.blue)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blue, fontSize: 18)),
+              Padding(
+                padding: EdgeInsets.only(top: 20, bottom: 20),
+                child: Container(
+                  height: 40,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    color: Colors.blue,
+                    child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 18),),
+                  )
+                ),
+              ),
+              Text(
+                "Valor por pessoa",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blue, fontSize: 18),
+              )
+            ],
+          )),
+        ),
+      )),
+    );
   }
-
-  Icon icon = new Icon(
-    Icons.monetization_on,
-    size: 120,
-    color: Colors.blue;
-  );
-
-  TextFormField formPessoas = TextFormField(
-    keyboardType: TextInputType.number,
-    decoration: InputDecoration(
-      labelText: "Quantidade de pessoas",
-      labelStyle: TextStyle(color: Colors.blue)
-    ),
-    textAlign: TextAlign.center,
-    style: TextStyle(color: Colors.blue, fontSize: 18)
-  );
-
-  TextFormField formConta = TextFormField(
-     keyboardType: TextInputType.number,
-     decoration: InputDecoration(
-       labelText: "Valor da conta",
-       labelStyle: TextStyle(color: Colors.blue)
-     ),
-    textAlign: TextAlign.center,
-    style: TextStyle(color: Colors.blue, fontSize: 18)
-  );
-
-  RaisedButton botaoCalcular = RaisedButton(
-    onPressed: () {},
-    color: Colors.blue,
-  );
-
-  Text textoValorPessoa = Text(
-    "Valor por pessoa",
-    textAlign: TextAlign.center,
-    style: TextStyle(color: Colors.blue, fontSize: 18),
-)
-
 }
